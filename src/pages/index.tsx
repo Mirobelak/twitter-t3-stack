330870159af4737ca745cf754690ac2472e8fef2
Miroslav Belak <miroslavbelak123@gmail.com>
2023-03-24 10:48:32 +0100

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { SignIn, useUser } from "@clerk/clerk-react";
 import { SignInButton, SignOutButton } from "@clerk/nextjs";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -13,19 +15,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      {!user.isSignedIn && ( 
-      <SignInButton mode="modal">
-     <button className="btn text-white">
-       Sign in
-     </button>
-   </SignInButton>)}
-   {!!user.isSignedIn && (
-   <SignOutButton>
-    <button className="btn text-white">
-       Sign Out
-     </button>
-   </SignOutButton>)}
-<SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        {!user.isSignedIn && (
+          <SignInButton mode="modal">
+            <button className="btn text-white">Sign in</button>
+          </SignInButton>
+        )}
+        {!!user.isSignedIn && (
+          <SignOutButton>
+            <button className="btn text-white">Sign Out</button>
+          </SignOutButton>
+        )}
+        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );

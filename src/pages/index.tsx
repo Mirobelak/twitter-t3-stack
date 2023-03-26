96d04f2f9ebb216795d4d6149e906b5087e8ae9a
Miroslav Belak <miroslavbelak123@gmail.com>
2023-03-26 10:41:31 +0200

    working posts func

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,24 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage} from "~/components/Loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
 
-
   const { user } = useUser();
+
+  const [input, setInput] = useState<string>("");
+
+  const ctx = api.useContext();
+
+  const {mutate, isLoading: isPosting} = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("") // clear input on success
+     void ctx.posts.getAll.invalidate()}
+  });
+
 
   if (!user) return null;
 
@@ -30,7 +41,12 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type Some Emojis !"
         className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({content: input})}>Post</button>
     </div>
   );
 };
@@ -69,7 +85,7 @@ const Feed = () => {
 
   return(
     <div className="flex flex-col">
-            {data?.map((fullPost) => (
+            {data.map((fullPost) => (
               <PostView {...fullPost} key={fullPost.post.id} />
             ))}
     </div>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,8 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import type { User } from "@clerk/nextjs/dist/api";
+import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 
 const filterUserForClient = (user: User) => {
@@ -14,7 +15,8 @@ const filterUserForClient = (user: User) => {
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.prisma.post.findMany({ take: 100 });
+
+    const posts = await ctx.prisma.post.findMany({ take: 100, orderBy: { createdAt: "desc" } });
 
     const users = (
       await clerkClient.users.getUserList({
@@ -24,18 +26,37 @@ export const postsRouter = createTRPCRouter({
     ).map(filterUserForClient);
 
     return posts.map((post) => {
+      
       const author = users.find((user) => user.id === post.authorId);
 
-      if (!author)
+      if (!author || !author.username)
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
-          message: "Author not found",
+          message: "Author post not found",
         });
 
-      return {
+      return {  
         post,
-        author,
+        author: {
+          ...author,
+          username: author.username, 
+        }
       };
     });
   }),
+
+  create: privateProcedure.input(z.object({
+    content: z.string().emoji().min(1).max(280),
+  })).mutation(async ({ ctx, input }) => {
+
+    const authorId = ctx.userId;
+
+    const post = await ctx.prisma.post.create({
+      data: {
+        authorId,
+        content: input.content,
+      },
+    });
+    return post;
+  })
 });
